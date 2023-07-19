d3c2e7339c55189b4ca0b371ddeff4a271ee5bdb
Adam Harrison <adamdharrison@gmail.com>
2023-07-19 13:31:56 -0400

    Fixed clean exit.

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 
 CFLAGS="$CFLAGS -fPIC -Ilib/lite-xl/resources/include"
 
-[[ "$@" == "clean" ]] && rm -f *.so *.dll
+[[ "$@" == "clean" ]] && rm -f *.so *.dll && exit 0
 $CC $CFLAGS src/*.c $@ -shared -o $BIN
