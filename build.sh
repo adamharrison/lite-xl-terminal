a78541de0904398c40374395536d4589651e1c6c
Adam Harrison <adamdharrison@gmail.com>
2023-11-20 15:46:25 -0500

    Adding implicitly included library.

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@
 CFLAGS="$CFLAGS -fPIC -Ilib/lite-xl/resources/include"
 
 [[ "$@" == "clean" ]] && rm -f *.so *.dll && exit 0
-$CC $CFLAGS src/*.c $@ -shared -o $BIN
+$CC $CFLAGS src/*.c $@ -shared -o $BIN -lutil
